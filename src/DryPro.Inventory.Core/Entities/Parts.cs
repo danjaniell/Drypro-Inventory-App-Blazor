6adb51e59d5497eace5221e388a725a9c99d8761
Jai <iaj.paragas@yahoo.com>
2022-03-08 20:55:46 +0800

    Added annotations to Parts entity

--- src/DryPro.Inventory.Core/Entities/Parts.cs
+++ src/DryPro.Inventory.Core/Entities/Parts.cs
@@ -10,10 +10,12 @@ namespace DryPro.Inventory.Core.Entities
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [DisplayName("Name")]
+        [StringLength(55)]
         public string Name { get; set; }
         [DisplayName("Description")]
+        [StringLength(255)]
         public string Description { get; set; }
         [DisplayName("Stock Items")]
-        public string StockItems { get; set; }
+        public int StockItems { get; set; }
     }
 }
